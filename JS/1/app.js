630cda201a201f2700eef4465dfbfc10931ea59f
unknown <pnn1999pnn@gmail.com>
2023-04-27 09:08:42 +0330

    First done!

---
+++ JS/1/app.js
@@ -0,0 +1,15 @@
+
+
+test() 
+// number because => the defualt type of num is number
+test(undefined) 
+// number 
+test('') 
+// string  => change our num type to string
+test(null) 
+// object=> type of null is already object
+
+
+
+
+
---
+++ JS/2/app.js
@@ -0,0 +1,22 @@
+function PassbyReference(obj) {
+    let tmp = obj.a;
+    obj.a = obj.b;
+    obj.b = tmp;
+    console.log(`Inside Pass By Reference
+    Function -> a = ${obj.a} b = ${obj.b}`);
+}
+let obj = { a: 10, b: 20 }
+console.log(`Before calling Pass By Reference
+    Function -> a = ${obj.a} b = ${obj.b}`);
+PassbyReference(obj)
+console.log(`After calling Pass By Reference
+    Function -> a = ${obj.a} b = ${obj.b}`);
+
+
+//////////////////////////////////////////////
+// 44444444444444444444444444444444
+// var rotator = function () {
+//     widget.Rotator.rotate();
+//     setTimeout(rotator, 5000);
+// };
+// rotator();
