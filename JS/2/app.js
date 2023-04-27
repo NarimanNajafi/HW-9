b01a6c4ec6ced611ea6449462713c263f501bfc5
unknown <pnn1999pnn@gmail.com>
2023-04-27 09:11:01 +0330

    2 Done!

--- JS/2/app.js
+++ JS/2/app.js
@@ -1,3 +1,17 @@
+// function PassbyReference(obj) {
+//     let tmp = obj.a;
+//     obj.a = obj.b;
+//     obj.b = tmp;
+//     console.log(`Inside Pass By Reference
+//     Function -> a = ${obj.a} b = ${obj.b}`);
+// }
+// let obj = { a: 10, b: 20 }
+// console.log(`Before calling Pass By Reference
+//     Function -> a = ${obj.a} b = ${obj.b}`);
+// PassbyReference(obj)
+// console.log(`After calling Pass By Reference
+//     Function -> a = ${obj.a} b = ${obj.b}`);
+//////////////////////////////////////////////// 
 function PassbyReference(obj) {
     let tmp = obj.a;
     obj.a = obj.b;
@@ -8,15 +22,7 @@ function PassbyReference(obj) {
 let obj = { a: 10, b: 20 }
 console.log(`Before calling Pass By Reference
     Function -> a = ${obj.a} b = ${obj.b}`);
-PassbyReference(obj)
+PassbyReference({ ...obj })
 console.log(`After calling Pass By Reference
     Function -> a = ${obj.a} b = ${obj.b}`);
 
-
-//////////////////////////////////////////////
-// 44444444444444444444444444444444
-// var rotator = function () {
-//     widget.Rotator.rotate();
-//     setTimeout(rotator, 5000);
-// };
-// rotator();
